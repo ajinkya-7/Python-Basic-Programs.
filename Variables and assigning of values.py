069fef4d8df16a68f12174bdf55229fb96047d0a
ajinkya-7 <72085714+ajinkya-7@users.noreply.github.com>
2020-12-22 17:01:12 +0530

    Add files via upload

---
+++ Variables and assigning of values.py
@@ -0,0 +1,23 @@
+#one way to assign a variable:
+
+name = "ronaldo"
+age = 35
+print(name)
+print(age)
+
+
+#multiple values and multiple variables in single line.
+
+name, age = "ronaldo", 35
+print(age)
+
+
+#single value to multiple variables
+
+number1 = number2 = number3 = number4 = 60
+number4 = 40
+number3 = number4
+number2 = number3
+number1 = number2
+print(number2)
+print(number1)
---
+++ while loop.py
@@ -0,0 +1,23 @@
+# eg:1
+
+a=0
+while a<=10:
+    print("the value of a is ",a)
+    a+=1
+
+    
+# eg:2
+
+a=3
+while a<=25:
+    print("Save trees",a)
+    a+=7
+
+
+# eg:3
+
+a=651561561
+while a<=6156156151:
+    print(a)
+    a+=65151
+##this get executes upto n number of times
